c0521e7c11cfa1ca7f9d2f0953fe0502fd81a672
Bruno Okamoto <obrunokenji@gmail.com>
2026-03-02 23:06:23 -0300

    i did activities

---
+++ ACTIVITIES/calcular_gorjeta/app.py
@@ -0,0 +1,17 @@
+def calcular_gorjeta():
+    try:
+        valor_conta = float(input('Digite o valor da conta: '))
+        porcentagem_gorjeta = float(input('Digite a porcentagem de gorjeta: '))
+        valor_novo = valor_conta * (1 + (porcentagem_gorjeta/100))
+        valor_gorjeta = valor_conta * (porcentagem_gorjeta/100)
+        print(f'Valor da gorjeta: ${valor_gorjeta:.2f}')
+        print(f'Total a pagar: ${valor_novo:.2f}')
+        
+    except ValueError:
+        print('Digite numeros')
+
+def main():
+    calcular_gorjeta()
+
+if __name__ == '__main__':
+    main()
